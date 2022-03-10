82da3598ea2f97d94e0ebde3dc2ac299b589ee3c
Tyagiharshit59 <harshit.tyagi@neosoftmail.com>
2022-03-10 13:06:51 +0530

    first commit

---
+++ slider/slider.js
@@ -0,0 +1,29 @@
+const imgWrapper = document.querySelector(".img-wrapper");
+const prevBtn =document.querySelector("#prev");
+const nextBtn =document.querySelector("#next");
+
+const img = document.querySelectorAll("img");
+
+const imgWidth = img[0].clientWidth;
+
+let index = 0;
+
+nextBtn.addEventListener("click", () => {
+        if (index < 2) {
+            index++;
+            imgWrapper.style.transform = `translateX(-${index * imgWidth}px)`;
+        } else {
+            index = 0;
+            imgWrapper.style.transform = `translateX(0px)`;
+        }
+    });
+
+prevBtn.addEventListener("click", () => {
+    if (index > 0) {
+      index--;
+      imgWrapper.style.transform = `translateX(-${index * imgWidth}px)`;
+    } else {
+      index = img.length - 1;
+      imgWrapper.style.transform = `translateX(-${index * imgWidth}px)`;
+    }
+  });
